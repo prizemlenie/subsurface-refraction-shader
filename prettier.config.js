e4b767f0b1f7a07de27b93472235f6ef5be790ab
prizemlenie <prizemlenie@gmail.com>
2025-01-03 18:43:28 +0100

    gh pages build

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-    printWidth: 80,
-};
--- src/js/gui.js
+++
@@ -1,9 +0,0 @@
-import { Pane } from 'tweakpane';
-import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
-
-export const pane = new Pane();
-pane.registerPlugin(EssentialsPlugin);
-export const info = pane.addFolder({ title: 'Info', expanded: false });
-export const settings = pane.addFolder({ title: 'Settings', expanded: false });
-export const nextBtn = settings.addButton({ title: 'Next mesh >>', index: 0 });
-settings.addBlade({ view: 'separator', index: 1 });
